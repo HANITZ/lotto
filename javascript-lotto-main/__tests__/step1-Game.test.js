9e9af508e9e6812b9f25da31dbe86cbaf39f3e03
장한울 <swingjhw@naver.com>
2023-04-23 19:05:42 +0900

    refactor(Game-step1): Game 함수 실행, 배열 반환, 구매금액 받아오기 수정

--- javascript-lotto-main/__tests__/step1-Game.test.js
+++ javascript-lotto-main/__tests__/step1-Game.test.js
@@ -5,9 +5,9 @@ describe('Game 함수 테스트', ()=>{
     beforeEach(()=>(
         game = Game()
     ))
+
     
     test('Game 함수가 실행이 되면서 6개의 로또 번호를 가져옴 ', () => {
-        console.log(game)
         const winningNumber = game.winningNumber
         expect(winningNumber.length).toEqual(6)
     })
--- javascript-lotto-main/src/js/game/step1-Game.js
+++ javascript-lotto-main/src/js/game/step1-Game.js
@@ -1,11 +1,12 @@
 const Game = () => {
-
+    const AmountOfBuy = 0
     const winningNumber = [1, 2, 3, 4, 5, 6]
     const bonusNumber = [7]
     
-
+    
     
     return {
+        AmountOfBuy,
         winningNumber,
         bonusNumber,
     }
