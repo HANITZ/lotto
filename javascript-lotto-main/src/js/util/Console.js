44517ff0bca2a9de8d091502bf55bd5a98febece
장한울 <swingjhw@naver.com>
2023-04-23 19:20:50 +0900

    feat(Console): 입력 Console 구현

---
+++ javascript-lotto-main/src/js/util/Console.js
@@ -0,0 +1,23 @@
+const { resolve } = require('path');
+
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const Console = {
+    readLine(inputMessage) {
+        return new Promise( resolve => {
+            rl.question(inputMessage, input => resolve(input))
+        })
+    },
+    print(message){
+        console.log(message)
+    },
+    close(){
+        rl.close();
+    }
+}
+
+module.exports = Console;
+
