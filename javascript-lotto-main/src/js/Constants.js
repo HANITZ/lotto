ae919fda536b19cd146bfc88d0cd16a1686b0d42
장한울 <swingjhw@naver.com>
2023-04-26 18:34:32 +0900

    feat(Constants): 당첨내역 메세지 업데이트

--- javascript-lotto-main/src/js/Constants.js
+++ javascript-lotto-main/src/js/Constants.js
@@ -10,7 +10,15 @@ const constants = {
         ISALLNUMBER: /^[0-9]*$/,
         ISALLNUMBERANDREST: /^[0-9|,]*$/g,
 
+    },
+    winnedLotto: {
+        ONE : '1등 당첨 6개 일치 - 2,000,000,000원',
+        TWO : '2등 당첨 5개 + 보너스 번호 일치 - 30,000,000원',
+        THREE : '3등 당첨 5개 일치 - 1,500,000원',
+        FOUR : '4등 당첨 4개 일치 - 50,000원',
+        FIVE : '5등 당첨 3개 일치 - 5,000원'
     }
+
 }
 
 export default constants;
