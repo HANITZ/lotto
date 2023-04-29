afc20778b54c74509ef362cbd7852660e97d9cb4
장한울 <swingjhw@naver.com>
2023-04-29 18:06:18 +0900

    feat: domList

---
+++ javascript-lotto-main/src/js/View/domList.js
@@ -0,0 +1,11 @@
+import { $ } from "../util/selector"
+
+
+const domList = {
+    buyBtn : $('.buy-btn'),
+    moneyInput: $('.money-imput'),
+    moneyInputErrorText: $('.money-input-error-text'),
+    mainContainer: $('.main-container'),    
+};
+
+export default domList;
