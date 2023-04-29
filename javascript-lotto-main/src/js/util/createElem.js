50538ab189d8e7c606611a6f5dd377e0778af297
장한울 <swingjhw@naver.com>
2023-04-29 18:02:11 +0900

    feat: preventFormFromSubmitting

---
+++ javascript-lotto-main/src/js/util/createElem.js
@@ -0,0 +1,8 @@
+const createElem = (tagName, type, name) => {
+  const elem = document.createElement(tagName);
+  if (type === "class") elem.className = name;
+  if (type === "id") elem.id = name;
+  return elem;
+};
+
+export default createElem;
--- javascript-lotto-main/step2.config.js
+++ javascript-lotto-main/step2.config.js
@@ -14,9 +14,6 @@ module.exports = {
   },
   resolve: {
     extensions: [".js", ".mjs", ".css"],
-    alias: {
-      '@lotto' : path.resolve(__dirname, 'src/js/')
-    }
   },
   module: {
     rules: [
